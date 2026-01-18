0cb5756e9729e2c5fdcfe0cc53708d10c3696199
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-18 13:27:16 -0500

    chore: change content

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     return (
         <div className="grow flex flex-col items-center justify-center gap-6">
             <h2 className="text-4xl md:text-6xl font-sora font-bold text-center">404</h2>
-            <p className="font-reddit-sans text-white/75 text-center">Sorry, but I have not found the page you are looking for.</p>
+            <p className="font-reddit-sans text-white/75 text-center">I am sorry, but I have not found the page you are looking for.</p>
             <Link
                 className="font-reddit-sans hover:font-bold hover:underline transition-all duration-500 ease-out text-center"
                 href='/'
