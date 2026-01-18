d256aac31670833d7fc5c10b95d7593dcafe20c2
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-17 23:00:13 -0500

    chore: add layout for blog page

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="mt-[1.25em] mb-[1.25em]">
+            {children}
+        </div>
+    )
+}
